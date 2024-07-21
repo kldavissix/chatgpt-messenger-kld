8742cae61c2dc6d36e164450cd8d4631e9e76ac4
Keith Lamont Davis <keithdaviswebdev@gmail.com>
2024-07-20 18:32:28 -0700

    Updated queryApi to match changes to ChatGPT API

--- lib/queryApi.ts
+++ lib/queryApi.ts
@@ -1,17 +1,18 @@
 import openai from "./chatgpt"
 
 const query = async (prompt: string, chatId: string, model: string) => {
-  const res = await openai.completions
+  const res = await openai.chat.completions
+
     .create({
       model,
-      prompt,
+      messages: [{ role: "user", content: prompt }],
       temperature: 0.9,
       top_p: 1,
       max_tokens: 1000,
       frequency_penalty: 0,
       presence_penalty: 0,
     })
-    .then((res) => res.choices[0].text)
+    .then((res) => res.choices[0].message.content)
     .catch(
       (err) =>
         `ChatGPT was unable to find an answer for that! (Error: ${err.message})`
